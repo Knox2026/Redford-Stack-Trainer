8f3c905ccf8c764d3c1fae5f8c732f2185403860
Knox2026 <keithrunion@gmail.com>
2026-03-21 12:42:14 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
-self.addEventListener('fetch', (event) => {});
+const CACHE_NAME = 'stack-trainer-v1.0';
+const ASSETS = [
+  'index.html',
+  'help.html',
+  'manifest.json',
+  'rsl.png',
+  'sw.js'
+];
+
+// Install: Cache all files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate: Remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Serve from cache first, then network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
